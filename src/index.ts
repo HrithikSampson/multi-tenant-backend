e1b88f0cab2960af234eff18ba5531ba83d3a0a7
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-09-27 22:20:26 +0530

    Remove problematic OPTIONS route handler

--- src/index.ts
+++ src/index.ts
@@ -35,16 +35,6 @@ const corsOptions: CorsOptions = {
 
 app.use(morgan(':date[iso] :method :url :status :response-time ms - :res[content-length]'));
 app.use(cors(corsOptions));
-app.options('/*', (req, res) => {
-  // unconditional preflight response (bullet-proof)
-  const origin = req.headers.origin as string | undefined;
-  if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
-  res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  return res.status(204).end();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
