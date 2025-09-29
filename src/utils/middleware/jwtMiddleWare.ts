46077e1f5b72616490f62d92ea5c50319e8b8b16
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-09-30 01:15:57 +0530

    feat: refresh features

--- src/utils/middleware/jwtMiddleWare.ts
+++ src/utils/middleware/jwtMiddleWare.ts
@@ -1,12 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
 import { AppDataSource } from '../../config/database';
 import { QueryRunner } from 'typeorm';
-
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
 
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'default-access-secret-for-demo';
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'default-refresh-secret-for-demo';
